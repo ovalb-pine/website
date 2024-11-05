79da5be4cb19f1c52cc557d070004f7faef1f593
ovalb-pine <maalefremova@edu.hse.ru>
2024-11-05 13:32:27 +0300

    make code more compact

---
+++ buttons.js
@@ -0,0 +1,6 @@
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+function bottomFunction() {
+window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})
+}
