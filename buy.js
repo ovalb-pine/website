2e4c1d6af31881c3286876a2de699f4b286ad3f2
ovalb-pine <maalefremova@edu.hse.ru>
2024-12-01 11:37:15 +0300

    websitre

--- buy.js
+++ buy.js
@@ -17,10 +17,11 @@ async function getResponce() {
         node_for_insert.innerHTML += `
         <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
         <img style="width: 180px" class="align-self-center" src=${content[key].img}>
-        <h5 class="card-title">${content[key].title}</h5>
-        <p class="card-text">${content[key].description}. Цена ${content[key].price} р.</p>
+        <h4 class="card-title">${content[key].title}</h5>
+        <p class="card-text\">${content[key].description}.</p>
+        <p><i>Price ${content[key].price}₽.</i></p>
         <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
-        <p class="card-text" >Заказать <input class="w-25" type="number" name="amount" value="0"></p>
+        <p class="card-text" >Order <input class="button field" type="number" name="amount" value="0"></p>
         </li>`
     }
 }
